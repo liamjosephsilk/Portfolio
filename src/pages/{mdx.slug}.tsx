d4e2b397beefbcc4fd6fa3821dc60605d2273e1c
Liam Silk <liamjosephsilk@gmail.com>
2021-02-15 12:48:19 +0000

    Scroll progress hook and component

--- src/pages/{mdx.slug}.tsx
+++ src/pages/{mdx.slug}.tsx
@@ -1,9 +1,11 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import styled from 'styled-components'
 import MDXWrapProvider from '../context/MDX/MDXWrapProvider'
-import { Heading } from '@atoms'
+import { Heading, PageProgress } from '@atoms'
+import useScrollPrgress from '../hooks/useScrollProgress'
+import useScrollProgress from '../hooks/useScrollProgress'
 
 const Wrapper = styled.article`
     width: 50%;
@@ -29,12 +31,15 @@ const ArticlePage: FC = ({ data }) => {
         frontmatter: { title }
     } = data.mdx
 
+    const scrollProgress = useScrollProgress();
+
     return (
         <>
             <MDXWrapProvider>
                 <Wrapper>
-                    <Heading weight="900" size="30px" line="1.3" color="#F2203E">{title}</Heading>
-                    <MDXRenderer>{body}</MDXRenderer>
+                  <PageProgress scrollPercentage={scrollProgress}/>
+                  <Heading weight="900" size="30px" line="1.3" color="#F2203E">{title}</Heading>
+                  <MDXRenderer>{body}</MDXRenderer>
                 </Wrapper>
             </MDXWrapProvider> 
         </>
